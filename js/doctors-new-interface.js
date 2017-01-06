80591877fb9481aabec0daad4fc7cae8610c8982
SuziRubino <suzi@suzirubi.com>
2017-01-06 15:49:46 -0800

    clear doctor list after appending text on resubmit of condition

--- js/doctors-new-interface.js
+++ js/doctors-new-interface.js
@@ -11,23 +11,28 @@ $(document).ready(function() {
     var userIssue = $('#issue').val();
 
     $('#issue').val("");
+    $("#appendDr").remove();
+
+    $('.showIssue').text(userIssue);
 
     $('.showUserIssue').show();
+
     $('.showDoctorList').show();
 
     $('.showIssue').text(userIssue);
 //begin backend
-    // var firstName = "";
-    // var lastName = "";
+
+    var imageDr = " ";
     var name = " ";
     var address = " ";
     var cityState = " ";
-    var imageDr = " ";
     var phone = " ";
 
 
     $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ userIssue +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
     .then(function(doctorList) {
+
+    imageDr = doctorList.data[0].profile.image_url;
 
     name = (doctorList.data[0].profile.first_name) + " " + (doctorList.data[0].profile.last_name);
     address = (doctorList.data[0].practices[0].visit_address.street)
@@ -35,22 +40,14 @@ $(document).ready(function() {
     cityState = (doctorList.data[0].practices[0].visit_address.city)+ ", " + (doctorList.data[0].practices[0].visit_address.state);
     console.log(cityState);
 
-    imageDr = doctorList.data[0].profile.image_url;
-    $('.doctorList').prepend('<li><img src="'+ imageDr + '" alt="doctor photo"/>');
-
     phone = doctorList.data[0].practices[0].phones[0].number;
 
-    // var phoneArray = ;
-
-
-    $('.doctorList').append("<li> Dr. " + name + "</li><li>" + address + "</li><li>" + cityState + "</li><li>" + phone + "</li>");
-
-    console.log(phone);
-
+    $('.doctorList').append('<ul id="appendDr"><li><img src="'+ imageDr + '" alt="doctor photo"/></li><li> Dr. ' + name + '</li><li>' + address + '</li><li>' + cityState + '</li><li>' + phone + '</li></ul>');
 
     })
     .fail(function(error){
-    console.log("fail");
+    console.log("fail")
+    alert("please enter a condition");
     });
   });
 });
