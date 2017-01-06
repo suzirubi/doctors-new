230336d2fae7b7e7fc811b61becf85eee86b508d
SuziRubino <suzi@suzirubi.com>
2017-01-06 14:42:23 -0800

    finish pulling properties from Better Doctors response

--- js/doctors-new-interface.js
+++ js/doctors-new-interface.js
@@ -1,7 +1,6 @@
 var apiKey = require('./../.env').apiKey;
 var Doctor = require('./../js/doctors-new.js').doctorModule;
 
-console.log("doctor interface");
 
 
 $(document).ready(function() {
@@ -10,15 +9,36 @@ $(document).ready(function() {
     event.preventDefault();
 
     var userIssue = $('#issue').val();
+
     $('#issue').val("");
-    $('.showIssue').text("Your issue is " + userIssue + ".");
+
+    $('.showUserIssue').show();
+    $('.showDoctorList').show();
+
+    $('.showIssue').text(userIssue);
+//begin backend
+    // var firstName = "";
+    // var lastName = "";
+    var name = " ";
+    var address = " ";
+    var cityState = " ";
+
     $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ userIssue +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
- .then(function(result) {
-    console.log(result);
-  })
- .fail(function(error){
+    .then(function(doctorList) {
+    console.log(doctorList);
+
+    name = (doctorList.data[0].profile.first_name) + " " + (doctorList.data[0].profile.last_name);
+    address = (doctorList.data[0].practices[0].visit_address.street)
+
+    cityState = (doctorList.data[0].practices[0].visit_address.city)+ ", " + (doctorList.data[0].practices[0].visit_address.state);
+    console.log(cityState);
+
+
+    $('.doctorList').append("<li>" + name + "</li>");
+
+    })
+    .fail(function(error){
     console.log("fail");
+    });
   });
-  });
-  console.log("In document ready");
 });
--- js/doctors-new.js
+++ js/doctors-new.js
@@ -1,5 +1,19 @@
-function Doctor() {
-}
+// var apiKey = require('./../.env').apiKey;
 
 
-exports.doctorModule = Doctor;
+// function GetDoctor() {
+//   // this.doctorList = doctorList;
+// }
+
+
+// GetDoctor.prototype.list = function(){
+//   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ userIssue +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+// .then(function(doctorList) {
+// console.log(doctorList);
+// })
+// .fail(function(error){
+// console.log("fail");
+// });
+// }
+
+// exports.doctorModule = getDoctor;
