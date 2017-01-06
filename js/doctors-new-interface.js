a8bb4d9bd054da62c16ea5356c4966447a24d7d3
SuziRubino <suzi@suzirubi.com>
2017-01-06 15:26:48 -0800

    basic framework for data tested andstyled

--- js/doctors-new-interface.js
+++ js/doctors-new-interface.js
@@ -22,10 +22,12 @@ $(document).ready(function() {
     var name = " ";
     var address = " ";
     var cityState = " ";
+    var imageDr = " ";
+    var phone = " ";
+
 
     $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ userIssue +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
     .then(function(doctorList) {
-    console.log(doctorList);
 
     name = (doctorList.data[0].profile.first_name) + " " + (doctorList.data[0].profile.last_name);
     address = (doctorList.data[0].practices[0].visit_address.street)
@@ -33,8 +35,18 @@ $(document).ready(function() {
     cityState = (doctorList.data[0].practices[0].visit_address.city)+ ", " + (doctorList.data[0].practices[0].visit_address.state);
     console.log(cityState);
 
+    imageDr = doctorList.data[0].profile.image_url;
+    $('.doctorList').prepend('<li><img src="'+ imageDr + '" alt="doctor photo"/>');
 
-    $('.doctorList').append("<li>" + name + "</li>");
+    phone = doctorList.data[0].practices[0].phones[0].number;
+
+    // var phoneArray = ;
+
+
+    $('.doctorList').append("<li> Dr. " + name + "</li><li>" + address + "</li><li>" + cityState + "</li><li>" + phone + "</li>");
+
+    console.log(phone);
+
 
     })
     .fail(function(error){
